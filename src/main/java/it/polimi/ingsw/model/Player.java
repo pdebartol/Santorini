2980625a0cfdeb478f0b2ba9231b637a04e23d8b
marcoDige <61740653+marcoDige@users.noreply.github.com>
2020-03-28 20:31:09 +0100

    implementation and comments of the player class

--- src/main/java/it/polimi/ingsw/model/Player.java
+++ src/main/java/it/polimi/ingsw/model/Player.java
@@ -1,25 +1,33 @@
 package it.polimi.ingsw.model;
 
+import java.util.ArrayList;
+
+/**
+ * Player represents a player who is currently playing in the match.
+ * Username can't be changed once player has been created.
+ * @author marcoDige
+ */
 public class Player {
 
     //attributes
 
-    private String username;
-    private Worker[] workers;
+    private final String username;
+    private ArrayList<Worker> workers;
     private God god;
 
     //constructors
 
-    public Player(String username, Worker[] workers) {
+    /**
+     * Class constructors which constructs a Player with a specified username and workers.
+     * @param username the player username
+     * @param workers workers that player use to play
+     */
+    public Player(String username, ArrayList<Worker> workers) {
         this.username = username;
         this.workers = workers;
     }
 
     //methods
-
-    public void setUsername(String u){
-        this.username = u;
-    }
 
     public void setGod(God g){
         this.god = g;
@@ -29,7 +37,7 @@ public class Player {
         return username;
     }
 
-    public Worker[] getWorkers(){
+    public ArrayList<Worker> getWorkers(){
         return workers;
     }
 
@@ -37,23 +45,59 @@ public class Player {
         return god;
     }
 
-    public void addWorker(Worker w){
-
-    }
-
     public void removeWorker(Worker w){
-
+        workers.remove(w);
     }
 
+    /**
+     * This method allows to control if the player is blocked (if the player can't move, he loses).
+     * @return true or false to indicate if at least one worker which player use can move in at least one square
+     * compatible with rules or not
+     */
     public boolean canMove(){
+        for(int i = -1; i >= -1 && i <= 1; i++)
+            for(int j = -1; j >= -1 && j <= 1; i++)
+                if(!(i == 0 && j == 0)) {
+                    for (int k = 0; k < workers.size(); k++) {
+                        int x = workers.get(k).getCurrentSquare().getXPosition() + i;
+                        int y = workers.get(k).getCurrentSquare().getYPosition() + j;
+                        if (x >= 0 && x <= 4 && y >= 0 && x <= 4)
+                            if (god.getPower().checkMove(workers.get(k), god.getPower().getBoard().getTable()[x][y]))
+                                return true;
+                    }
+                }
         return false;
     }
 
+    /**
+     * This method holds all the logic behind a move. if the move is possible, it updates the model status. It also
+     * control if after the move, the player has won.
+     * @param w is the worker who wants to move
+     * @param s is the square where the worker wants to move
+     * @return true or false to indicate if the move was done or not
+     */
     public boolean move(Worker w, Square s){
+        Power power = god.getPower();
+        if(power.checkTurn(0) && power.checkMove(w,s)){
+            power.updateMove(w,s);
+            //TODO: notify if Player win after control with winChek()
+            return true;
+        }
         return false;
     }
 
+    /**
+     * This method holds all the logic behind a build move. If the build move is possible, it updates the model status.
+     * @param w is the worker who wants to build
+     * @param s is the square where the worker wants to build
+     * @return true or false to indicate if the build move was done or not
+     */
     public boolean build(Worker w, Square s){
+        Power power = god.getPower();
+        if(power.checkTurn(0) && power.checkBuild(w,s)){
+            power.updateBuild(w,s);
+            return true;
+        }
         return false;
     }
 }
