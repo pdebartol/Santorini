6985f1d16a05ea9a03c6e498df33f375268f91e0
pierobartolo <piersilvio.debartolomeis@mail.polimi.it>
2020-03-29 19:50:13 +0200

    Fixed typo in Player.canMove

--- src/main/java/it/polimi/ingsw/model/Player.java
+++ src/main/java/it/polimi/ingsw/model/Player.java
@@ -63,7 +63,7 @@ public class Player {
      */
     public boolean canMove(){
         for(int i = -1; i <= 1 ; i++)
-            for(int j = -1; j <= 1; i++)
+            for(int j = -1; j <= 1; j++)
                 if(!(i == 0 && j == 0)) {
                     for (Worker worker : workers) {
                         int x = worker.getCurrentSquare().getXPosition() + i;
