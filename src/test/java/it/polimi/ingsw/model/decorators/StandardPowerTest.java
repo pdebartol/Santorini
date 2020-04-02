cad54e4bb9dfd749b1de82af34feffd8fbd60966
Marco Di Gennaro <61740653+marcoDige@users.noreply.github.com>
2020-04-02 13:34:10 +0200

    partially written StandardPower test

--- src/test/java/it/polimi/ingsw/model/decorators/StandardPowerTest.java
+++ src/test/java/it/polimi/ingsw/model/decorators/StandardPowerTest.java
@@ -1,9 +1,6 @@
 package it.polimi.ingsw.model.decorators;
 
-import it.polimi.ingsw.model.Board;
-import it.polimi.ingsw.model.Color;
-import it.polimi.ingsw.model.Player;
-import it.polimi.ingsw.model.Worker;
+import it.polimi.ingsw.model.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -15,38 +12,86 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class StandardPowerTest {
 
+    Board b;
+    Player p1,p2,p3;
+    Worker wmp1,wfp1,wmp2,wfp2;
+    Power p;
+
     @BeforeEach
-    void setUp() {
-        Board b = new Board();
-        Player p = new Player("marcoDige", Color.AZURE);
-        Worker w1 = p.getWorkers().get(1);
-        Worker w2 = p.getWorkers().get(2);
+    public void setUp() {
+        b = new Board();
+        p1 = new Player("marcoDige", Color.AZURE);
+        p2 = new Player("pierobartolo", Color.WHITE);
+        p3 = new Player("aledimaio",Color.GREY);
+        wmp1 = p1.getWorkers().get(0);
+        wfp1 = p1.getWorkers().get(1);
+        wmp2 = p2.getWorkers().get(0);
+        wfp2 = p2.getWorkers().get(1);
 
-        w1.setWorkerOnBoard(b.getSquare(1,1));
-        w2.setWorkerOnBoard(b.getSquare(3,4));
+        wmp1.setWorkerOnBoard(b.getSquare(1,1));
+        wfp1.setWorkerOnBoard(b.getSquare(3,4));
+        wmp2.setWorkerOnBoard(b.getSquare(1,3));
+        wfp2.setWorkerOnBoard(b.getSquare(3,2));
+
+        p = new StandardPower(1,1, b);
     }
 
     @Test
-    void checkMove() {
+    public void checkMove() {
     }
 
     @Test
-    void checkBuild() {
+    public void checkBuild() {
     }
 
     @Test
-    void checkWin() {
+    public void checkWin() {
+        //creating a ladder to win (we test checkWin with wmp1)
+        Square l2,l3;
+        l2 = b.getSquare(2,0);
+        l3 = b.getSquare(3,0);
+        //level 2
+        for(int i = 0 ; i < 2 ; i++)
+            l2.buildLevel();
+        //level 3
+        for(int i = 0 ; i < 3 ; i++)
+            l3.buildLevel();
+
+        assertEquals(2,l2.getLevel());
+        assertEquals(3,l3.getLevel());
+        //set wmp1 on level 2 position;
+        wmp1.updateWorkerPosition(l2);
+        assertEquals(wmp1,l2.getWorker());
+        assertEquals(false,p.checkWin(wmp1));
+        //move wmp2 on level 3 position;
+        wmp1.updateWorkerPosition(l3);
+        assertEquals(wmp1,l3.getWorker());
+        assertEquals(true,p.checkWin(wmp1));
+
     }
 
     @Test
-    void updateMove() {
+    public void updateMove() {
+        //move wmp1 to 2,1
+        p.updateMove(wmp1,b.getSquare(2,1));
+        assertEquals(wmp1,b.getSquare(2,1).getWorker());
+        assertEquals(wmp1.getCurrentSquare(),b.getSquare(2,1));
+        assertEquals(b.getSquare(1,1),wmp1.getLastSquareMove());
+        assertEquals(1,b.getNMoves());
+        //move wmp1 to 2,2
+        p.updateMove(wmp1,b.getSquare(2,2));
+        assertEquals(wmp1,b.getSquare(2,2).getWorker());
+        assertEquals(wmp1.getCurrentSquare(),b.getSquare(2,2));
+        assertEquals(b.getSquare(2,1),wmp1.getLastSquareMove());
+        assertEquals(2,b.getNMoves());
     }
 
     @Test
-    void updateBuild() {
+    public void updateBuild() {
+
     }
 
     @Test
-    void checkTurn() {
+    public void checkTurn() {
     }
 }
