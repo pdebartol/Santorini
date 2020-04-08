7a728cf1f545da1e027a5c9e40ec5112e87b04fe
aledimaio <61753404+aledimaio@users.noreply.github.com>
2020-04-08 17:36:54 +0200

    Test for BuiltUnderFoot class with javadoc

--- src/test/java/it/polimi/ingsw/model/decorators/BuildUnderfootTest.java
+++ src/test/java/it/polimi/ingsw/model/decorators/BuildUnderfootTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,47 +19,103 @@ import static org.junit.jupiter.api.Assertions.*;
 class BuildUnderfootTest {
 
     Board b;
-    Player p1, p2, p3;
+    Player p1;
+
+    /**
+     * Setup for tests:
+     * - 1 Player
+     * - all worker set on board
+     */
 
     @BeforeEach
     void setUp() {
 
         b = new Board();
         GodsFactory factory = new GodsFactory(b);
-        p1 = new Player("pierobartolo", Color.WHITE);
-        p2 = new Player("marcodige",Color.GREY);
-        p3 = new Player("aledimaio",Color.AZURE);
-        ArrayList<God> gods = factory.getGods(new ArrayList<>(Arrays.asList(10,29,30)));
-        p1.setGod(Objects.requireNonNull(gods.get(0))); // Prometheus
-        p2.setGod(Objects.requireNonNull(gods.get(1))); // Triton
-        p3.setGod(Objects.requireNonNull(gods.get(2))); // Zeus
+        p1 = new Player("aledimaio", Color.WHITE);
+        ArrayList<God> gods = factory.getGods(new ArrayList<>(Collections.singletonList(30)));
+        p1.setGod(Objects.requireNonNull(gods.get(0))); // Zeus
 
         // p1 sets workers
         p1.getWorkers().get(0).setWorkerOnBoard(b.getSquare(0,0));
         p1.getWorkers().get(1).setWorkerOnBoard(b.getSquare(0,1));
 
-        // p2 sets workers
-        p2.getWorkers().get(0).setWorkerOnBoard(b.getSquare(4,4));
-        p2.getWorkers().get(1).setWorkerOnBoard(b.getSquare(4,2));
+    }
 
-        // p3 sets workers
-        p3.getWorkers().get(0).setWorkerOnBoard(b.getSquare(2,2));
-        p3.getWorkers().get(1).setWorkerOnBoard(b.getSquare(3,0));
+    /**
+     * This method tests if standard checkBuild still works
+     */
+
+    @Test
+    void checkStandardBuild() {
+
+        //worker moves to near square
+        p1.move(p1.getWorkers().get(0),1,1);
+        assertEquals(0, b.getSquare(1,1).getLevel());
+        //check if everything is ok
+        assertTrue(p1.getGod().getPower().checkBuild(p1.getWorkers().get(0),0,0,1).isEmpty());
+        assertFalse(p1.getGod().getPower().checkBuild(p1.getWorkers().get(0),0,0,2).isEmpty());
+
 
     }
 
+    /**
+     * This method verifies if Zeus's power works, both check and build has been tested
+     */
+
     @Test
-    void checkBuild() {
+    void checkBuildUnderFoot(){
 
-        assertEquals(p3.getWorkers().get(0), b.getSquare(2,2).getWorker());
-        assertEquals(0, b.getSquare(2,2).getLevel());
-        p3.getGod().getPower().checkMove(p3.getWorkers().get(0),2,1);
+        //worker moves to near empty square
+        p1.move(p1.getWorkers().get(0),1,1);
+        //check worker position
+        assertEquals(0, b.getSquare(1,1).getLevel());
+        //check that worker can build under its foot
+        assertTrue(p1.getGod().getPower().checkBuild(p1.getWorkers().get(0),1,1,1).isEmpty());
+        //check tha worker cannot build more than a level under its foot
+        assertFalse(p1.getGod().getPower().checkBuild(p1.getWorkers().get(0),1,1,2).isEmpty());
+        //effectively build and check everything is ok
+        p1.build(p1.getWorkers().get(0),1,1,1);
+        assertEquals(p1.getWorkers().get(0), b.getSquare(1,1).getWorker());
 
-        p3.getGod().getPower().updateMove(p3.getWorkers().get(0),2,1);
-        assertEquals(p3.getWorkers().get(0), b.getSquare(2,1).getWorker());
-        //assertEquals("[]", p3.getGod().getPower().checkBuild(p3.getWorkers().get(0),2,1,1));
+    }
 
-        p3.getGod().getPower().updateBuild(p3.getWorkers().get(0),2,2,1);
+    /**
+     * This method tests that a worker cannot build a dome under itself
+     */
+
+    @Test
+    void checkCannotBuildDomeUnderFoot(){
+
+        b.getSquare(0,0).buildLevel(3);
+        b.getSquare(1,1).buildLevel(3);
+        b.resetCounters();
+        //move and check if a dome can be build under worker's foot
+        p1.move(p1.getWorkers().get(0),1,1);
+        assertFalse(p1.getGod().getPower().checkBuild(p1.getWorkers().get(0), 1,1,4).isEmpty());
+
+    }
+
+    /**
+     * This method tests that a player cannot win if uses Zeus's power to go from a level two to a level 3
+     */
+
+    @Test
+    void checkWinConditionUnderFoot(){
+
+        b.getSquare(0,0).buildLevel(1);
+        b.getSquare(1,1).buildLevel(2);
+        b.resetCounters();
+
+        assertEquals(p1.getWorkers().get(0), b.getSquare(0,0).getWorker());
+        assertEquals(1, b.getSquare(0,0).getLevel());
+        p1.move(p1.getWorkers().get(0),1,1);
+        //build level 3
+        p1.build(p1.getWorkers().get(0),1,1,3);
+        assertEquals(3, b.getSquare(1,1).getLevel());
+        assertEquals(p1.getWorkers().get(0), b.getSquare(1,1).getWorker());
+        //check if Player win
+        assertFalse(p1.getGod().getPower().checkWin(p1.getWorkers().get(0)));
 
 
     }
