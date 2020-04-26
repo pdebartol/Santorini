82c60a9fe42a608a2e808e43ba23495c22001cd4
Marco Di Gennaro <61740653+marcoDige@users.noreply.github.com>
2020-04-26 04:44:55 +0200

    update ClientHandler (XML file communication now works)

--- src/main/java/it/polimi/ingsw/view/server/ClientHandler.java
+++ src/main/java/it/polimi/ingsw/view/server/ClientHandler.java
@@ -19,22 +19,31 @@ public class ClientHandler implements Runnable{
 
     @Override
     public void run() {
-        File file = new File("msg.xml");
+
+        File file = new File("src/main/resources/clientRequest.xml");
 
         try {
-            byte[] bytearray = new byte[(int) file.length()];
-            BufferedInputStream in = new BufferedInputStream(new FileInputStream(file));
-            in.read(bytearray, 0, bytearray.length);
-            OutputStream out = client.getOutputStream();
-            out.write(bytearray, 0, bytearray.length);
-            out.flush();
+            InputStream in = client.getInputStream();
+            FileOutputStream out = new FileOutputStream(file, false);
 
+            byte[] buffer = new byte[256];
+
+            for(int r = in.read(buffer); r != 1; r = in.read(buffer)) {
+                out.write(buffer, 0, r);
+            }
+
+            out.flush();
             out.close();
             in.close();
-            client.close();
+            file.delete();
         } catch (IOException e) {
             System.err.println(e.getMessage());
+        }finally {
+            try{
+                client.close();
+            }catch (IOException e){
+                System.err.println(e.getMessage());
+            }
         }
-
     }
 }
