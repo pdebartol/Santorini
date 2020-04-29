b77aec99e0f1d8cfe822fa06861c67eae3387979
aledimaio <61753404+aledimaio@users.noreply.github.com>
2020-04-29 09:30:43 +0200

    adjust name style of some Board class variables

--- src/main/java/it/polimi/ingsw/model/Board.java
+++ src/main/java/it/polimi/ingsw/model/Board.java
@@ -100,11 +100,11 @@ public class Board {
 
     public boolean isAdjacent(Square s1, Square s2){
         if( s1 == null || s2 == null) throw new IllegalArgumentException("Null square as argument!");
-        int xdist = s1.getXPosition() - s2.getXPosition();
-        int ydist = s1.getYPosition() - s2.getYPosition();
+        int xDist = s1.getXPosition() - s2.getXPosition();
+        int yDist = s1.getYPosition() - s2.getYPosition();
 
-        if(xdist == 0 && ydist == 0) return false; // Same square
-        return xdist >= -1 && xdist <= 1 && ydist >= -1 && ydist <= 1;
+        if(xDist == 0 && yDist == 0) return false; // Same square
+        return xDist >= -1 && xDist <= 1 && yDist >= -1 && yDist <= 1;
     }
 
 }
