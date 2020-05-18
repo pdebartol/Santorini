e9d2e3205953a482fd08b54b9543e4f6d9b7efe1
aledimaio <61753404+aledimaio@users.noreply.github.com>
2020-05-18 16:05:34 +0200

    added gui elements

---
+++ src/main/java/it/polimi/ingsw/view/client/gui/LoginUsernameController.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw.view.client.gui;
+
+import javafx.scene.input.MouseEvent;
+
+public class LoginUsernameController {
+
+    public void sendUsername(MouseEvent mouseEvent) {
+    }
+
+}
