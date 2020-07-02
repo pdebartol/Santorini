5370ed2e1821206e293d1bc3cb6fa4bca114eb34
Marco Di Gennaro <61740653+marcoDige@users.noreply.github.com>
2020-07-02 16:52:20 +0200

    ToDoMsgWriter javadoc done.

--- src/main/java/it/polimi/ingsw/msgUtilities/server/ToDoMsgWriter.java
+++ src/main/java/it/polimi/ingsw/msgUtilities/server/ToDoMsgWriter.java
@@ -9,13 +9,28 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.InputStream;
 import java.util.List;
 
+/**
+ * This class contains all the methods useful to write an XML schema message which represents an indication of what to
+ * do to a client from the server.
+ * @author marcoDige
+ */
+
 public class ToDoMsgWriter {
 
     //attributes
 
+    /**
+     * This attribute represents the XML abstract document
+     */
+
     private Document document;
 
     //constructor
+
+    /**
+     * This constructor initializes the document with the standard XML schema content in toDoMsg file (content in
+     * resources folder).
+     */
 
     public ToDoMsgWriter(){
         try {
@@ -30,6 +45,16 @@ public class ToDoMsgWriter {
 
     //support methods
 
+    /**
+     * This method allows to create a new tag into the XML schema with an attribute.
+     * @param father is the father tag
+     * @param tagName is the name of the new tag
+     * @param textContent is the content of the new tag
+     * @param attributeName is the name of the attribute
+     * @param attributeValue is the value of the attribute
+     * @return the tag's Node object
+     */
+
     private Node appendTagWithAttribute(Node father, String tagName, String textContent, String attributeName, String attributeValue){
         Element tag = document.createElement(tagName);
         tag.setAttribute(attributeName,attributeValue);
@@ -38,6 +63,14 @@ public class ToDoMsgWriter {
         return document.getElementsByTagName(tagName).item(0);
     }
 
+    /**
+     * This method allows to create a new tag into the XML schema.
+     * @param father is the father tag
+     * @param tagName is the name of the new tag
+     * @param textContent is the content of the new tag
+     * @return the tag's Node object
+     */
+
     private Node appendTag(Node father, String tagName, String textContent){
         Element tag = document.createElement(tagName);
         tag.setTextContent(textContent);
@@ -45,11 +78,24 @@ public class ToDoMsgWriter {
         return document.getElementsByTagName(tagName).item(0);
     }
 
+    /**
+     * This method allows to create a new tag into the XML schema (empty tag).
+     * @param father is the father tag
+     * @param tagName is the name of the new tag
+     * @return the tag's Node object
+     */
+
     private Node appendTag(Node father, String tagName){
         Element tag = document.createElement(tagName);
         father.appendChild(tag);
         return document.getElementsByTagName(tagName).item(0);
     }
+
+    /**
+     * This element initializes the tag to write us inside.
+     * @param tagName is the tag to initialize name
+     * @return the tag's Node object
+     */
 
     private Node initializeTagList(String tagName){
         return document.getElementsByTagName(tagName).item(0);
@@ -69,11 +115,24 @@ public class ToDoMsgWriter {
         return builder.parse(stream);
     }
 
+    /**
+     * This method writes an Action to do indication message
+     * @param action is the action that the client has to do
+     * @return the updated document
+     */
+
     public Document toDoAction(String action){
         Node actionTag = initializeTagList("Action");
         actionTag.setTextContent(action);
         return document;
     }
+
+    /**
+     * This method writes a wait to do indication message with the reason for waiting and who is acting.
+     * @param inActionPlayer is the player who is acting
+     * @param waitFor is the reason for waiting
+     * @return the updated document
+     */
 
     public Document toDoWaitMsg(String inActionPlayer,String waitFor){
         toDoAction("wait");
@@ -84,6 +143,13 @@ public class ToDoMsgWriter {
         return document;
     }
 
+    /**
+     * This method writes a Turn indication message. It informs the client that he must perform the turn
+     * he can start playing and what operation he can perform
+     * @param possibleOperation is the operation he can perform
+     * @return the updated document
+     */
+
     public Document toDoTurn(String possibleOperation){
         toDoAction("yourTurn");
         Node infoTag = initializeTagList("Info");
@@ -91,6 +157,12 @@ public class ToDoMsgWriter {
         appendTag(infoTag,"possibleOperation",possibleOperation);
         return document;
     }
+
+    /**
+     * This method writes a message to inform the client who has to chose his god.
+     * @param ids is the list of gods left to choose from
+     * @return the updated document
+     */
 
     public Document toDoChoseGod(List<Integer> ids){
         toDoAction("choseGod");
